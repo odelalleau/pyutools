e20646d459e57afe7b6547839ea903baff6805cf
Olivier Delalleau <delallea@iro>
2013-01-10 11:19:52 -0500

    Cleaner behavior of flattening script

--- src/pyutools/git/scripts/flatten.py
+++ src/pyutools/git/scripts/flatten.py
@@ -43,18 +43,92 @@ def my_exec(cmd):
     return execute(cmd, must_succeed=True)
 
 
+def analyze_branches(logger, clean_up=False, clean_up_idx_0=True):
+    """
+    Perform analysis of branches and potentially deletes some.
+
+    :param logger: Logger object.
+
+    :param clean_up_idx_0: Whether to also delete the work branch with index 0
+    (which typically contains the final output). Has no effect when `clean_up`
+    is False (in which case we do not delete branch).
+
+    :return: The current branch (None if not currently on a branch).
+    """
+    branches = my_exec('git branch')
+    current_branch = None
+    for branch in branches:
+        is_current = branch.startswith('*')
+        if is_current:
+            branch = branch[1:]
+            current_branch = branch.strip()
+        branch = branch.strip()
+        template = 'flatten_tmp_branch_'
+        if branch.startswith(template):
+            # This is a temporary branch previously created by this script.
+            branch_idx = int(branch[len(template):])
+            if clean_up:
+                if branch_idx == 0 and not clean_up_idx_0:
+                    # Keep branch with index 0 if we asked for it.
+                    continue
+                if is_current:
+                    raise RuntimeError(
+                            'You are currently on branch \'%s\' which needs '
+                            'to be deleted. Please first move to another '
+                            'branch.' % branch)
+                logger.debug('Deleting old temporary branch: %s' % branch)
+                my_exec('git branch -D %s' % branch)
+            else:
+                raise RuntimeError(
+                        'Found branch \'%s\' that is probably a leftover '
+                        'from a previous flattening attempt. Please run this '
+                        'script with the --clean option to delete it, or '
+                        'do it manually.')
+    return current_branch
+
+
 def flatten(start, end, state):
     """
     Flatten from commit `start` to commit `end`.
 
-    This is a recursive function, whose state is a storage object holding:
+    This was meant to be a recursive function, whose state is a storage object
+    holding:
         - logger: the current logger
         - branch_idx: the current maximum temporary branch index
+        - origin: name of the original branch we want to get back to after
+          flattening
 
-    :return: 0 on success, non-zero on failure.
+    However, the current implementation is not recursive, to keep things
+    simple. Making it recursive would be useful to avoid situations where a
+    huge branch may be collapsed to a single commit due to a conflict caused
+    by a minor side-branch, which could have been resolved by recursively
+    linearizing this huge branch. Such "recursivization" is left for future
+    work.
+
+    Before exiting, this function always restores the original branch found
+    in `state.origin` (even if an exception is raised).
+
+    :return: The name of the flattened branch.
+    """
+    # Wrapper around `_flatten` to restore the original branch.
+    try:
+        return _flatten(start, end, state)
+    except:
+        # Throw away local changes.
+        my_exec('git reset --hard %s' % state.origin)
+        raise
+    finally:
+        # Restore original branch.
+        my_exec('git checkout %s' % state.origin)
+
+
+def _flatten(start, end, state):
+    """
+    Actual implementation of `flatten`.
     """
     logger = state.logger
 
+    # Helper function to create a new branch with a unique name.
     def make_new_branch(commit):
         name = 'flatten_tmp_branch_%s' % state.branch_idx
         execute('git checkout -b %s %s' % (name, commit),
@@ -65,18 +139,17 @@ def flatten(start, end, state):
     # Work in a new temporary branch.
     base_branch = make_new_branch(end)
 
-    # First attempt a simple rebase. If it works then we are good to go.
-    if False:
-        logger.debug('Attempting a rebase on top of %s' % start)
-        r_code, stdout, stderr = execute('git rebase %s' % start,
-                                         return_stdout=True,
-                                         return_stderr=True)
-        if r_code == 0:
-            logger.debug('Rebase successful!')
-            return 0
+    # First attempt: simple rebase. If it works then we are good to go.
+    logger.debug('Attempting a rebase on top of %s' % start)
+    r_code, stdout, stderr = execute('git rebase %s' % start,
+                                     return_stdout=True,
+                                     return_stderr=True)
+    if r_code == 0:
+        logger.debug('Rebase successful!')
+        return base_branch
 
-        logger.debug('Rebase failure -- rolling back')
-        execute('git rebase --abort', must_succeed=True)
+    logger.debug('Rebase failure -- rolling back')
+    execute('git rebase --abort', must_succeed=True)
 
     # Get parent/child relationships.
     # The `data` dictionary maps a commit hash to a `Storage` instance holding:
@@ -85,7 +158,7 @@ def flatten(start, end, state):
     #   - its children
     logger.debug('Analyzing parent/child relationships')
     data = dict()
-    commits = execute('git rev-list --parents --reverse %s' % end,
+    commits = execute('git rev-list --parents --reverse --topo-order %s' % end,
                       must_succeed=True)
     for commit_info in commits:
         tokens = commit_info.split(' ')
@@ -167,6 +240,11 @@ def flatten(start, end, state):
                 # so as to be able to gather the list of commits being
                 # considered. We will combine their commit notes in the
                 # patch being committed.
+                # TODO This can probably be obtained in a simpler way through
+                # a command of the form ``git -rev-list --left-right A...B``
+                # but for now we are keeping this version since it seems to
+                # work (however the current version provides useless commit
+                # hashes in the logs).
                 logger.debug('Dummy rebase to gather list of commits')
                 my_exec('git rebase -X theirs %s' % head.hash)
                 patch_info = my_exec('git log %s..HEAD' % head.hash)
@@ -192,8 +270,6 @@ def flatten(start, end, state):
                     commit_f_name = '.tmp.flatten_msg'
                     commit_file = open(commit_f_name, 'w')
                     try:
-                        # TODO The commit hashes in the log are not very
-                        # helpful since they come from a dummy rebase.
                         commit_file.write(
                             'Automatic patch built from combined commits\n'
                             '\n'
@@ -211,7 +287,8 @@ def flatten(start, end, state):
         # Update current head.
         head = child
        
-    return 0
+    return work_branch
+
 
 def get_root_commits():
     """
@@ -291,28 +368,8 @@ def run(args, logger):
 
     # Ensure there is no leftover branch from a previous flattening attempt,
     # cleaning them as necessary.
-    branches = execute('git branch', must_succeed=True)
-    for branch in branches:
-        is_current = branch.startswith('*')
-        if is_current:
-            branch = branch[1:]
-        branch = branch.strip()
-        if branch.startswith('flatten_tmp_branch_'):
-            # This is a temporary branch previously created by this script.
-            if args.clean:
-                if is_current:
-                    raise RuntimeError(
-                            'You are currently on branch \'%s\' which needs '
-                            'to be deleted. Please first move to another '
-                            'branch.' % branch)
-                logger.debug('Deleting old temporary branch: %s' % branch)
-                execute('git branch -D %s' % branch, must_succeed=True)
-            else:
-                raise RuntimeError(
-                        'Found branch \'%s\' that is probably a leftover '
-                        'from a failed flattening attempt. Please run this '
-                        'script with the --clean option to delete it, or '
-                        'do it manually.')
+    current_branch = analyze_branches(logger, clean_up=args.clean)
+    assert current_branch is not None
 
     # Identify the repository root. Currently we only support a single root.
     roots = get_root_commits()
@@ -326,11 +383,16 @@ def run(args, logger):
     root = roots[0]
     logger.debug('Found root commit: %s' % root)
 
-    # Initialize recursion: flatten from first commit to HEAD.
-    state = util.Storage(logger=logger, branch_idx=0)
-    rval = flatten(root, 'HEAD', state)
+    # Flatten from first commit to HEAD.
+    state = util.Storage(logger=logger, branch_idx=1, origin=current_branch)
+    flattened_branch = flatten(root, 'HEAD', state)
+    # Save the result and clean up temporary work branch.
+    if flattened_branch is not None:
+        my_exec('git checkout -b flatten_tmp_branch_0 %s' %
+                flattened_branch)
+        analyze_branches(logger, clean_up=True, clean_up_idx_0=False)
 
-    return rval
+    return 0
 
 
 if __name__ == '__main__':
